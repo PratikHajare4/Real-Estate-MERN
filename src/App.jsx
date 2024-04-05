a7041499d39c896be9eaeb68a6001c19d8445545
Pratik <hajarepratik10@gmail.com>
2024-04-05 23:36:27 +0530

    adding project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div className='flex flex-row items-center justify-center '>
+    <div className='text-2xl text-red-600   '>
+      App
+    </div>
+    </div>
+  )
+}
