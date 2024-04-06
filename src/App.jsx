175b04da9def63587ace1a95184a9028182d8fbd
Pratik <hajarepratik10@gmail.com>
2024-04-06 15:55:47 +0530

    Added Header section

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,25 @@ import SignUp from './pages/SignUp'
 import About from './pages/About'
 import Profile from './pages/Profile'
 import SignIn from './pages/SignIn'
+import Header from './components/Header'
+import Benefits from './pages/Benefits'
+import Residence from './pages/Residence'
+import ContactUs from './pages/ContactUs'
  
 
 
 export default function App() {
   return <BrowserRouter>
+  <Header/>
   <Routes>
 <Route path="/" element={<Home/>} />
 <Route path="/sign-in" element={<SignIn/>} />
 <Route path="/sign-up" element={<SignUp/>} />
 <Route path="/about" element={<About/>} />
 <Route path="/profile" element={<Profile/>} />
-<Route path="/" element={<Home/>} />
+<Route path="/benefits" element={<Benefits/>} />
+<Route path="/residence" element={<Residence/>} />
+<Route path="/contact" element={<ContactUs/>} />
 
   </Routes>
   </BrowserRouter>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div className='flex flex-row items-center justify-center '>
-      <div className='text-2xl text-black bg-slate-500 pt-10'>
+    <div className=' '>
+      <div className='text-2xl'>
       Home
       </div>
     </div>
